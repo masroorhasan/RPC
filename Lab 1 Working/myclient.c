16bd8e5a6ee2f0f6ecbc0563d92692c4d5f39377
Warren Smith <sharpsw@gmail.com>
2014-05-28 22:26:16 -0400

    Style on client

--- Lab 1 Working/myclient.c
+++ Lab 1 Working/myclient.c
@@ -3,9 +3,9 @@
  *
  * http://www.cs.rutgers.edu/~pxk/417/notes/sockets/udp.html
  * http://stackoverflow.com/questions/9778806/serializing-a-class-with-a-pointer-in-c
- * 
+ *
  * Coding Style:
- * 
+ *
  * http://www.cs.swarthmore.edu/~newhall/unixhelp/c_codestyle.html
  */
 
@@ -63,12 +63,12 @@ return_type make_remote_call(const char *servernameorip,
 
     // Create client socket and bind address to it
     int client_socket = socket(AF_INET, SOCK_DGRAM, 0);
-	struct sockaddr_in client_socket_address;
-	memset((char *)&client_socket_address, 0, sizeof(client_socket_address));
-	client_socket_address.sin_addr.s_addr = htonl(INADDR_ANY);
-	client_socket_address.sin_family = AF_INET;
+   	struct sockaddr_in client_socket_address;
+	  memset((char *)&client_socket_address, 0, sizeof(client_socket_address));
+	  client_socket_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	  client_socket_address.sin_family = AF_INET;
     client_socket_address.sin_port = htons(client_port);
-	bind(client_socket, (struct sockaddr *)&client_socket_address, sizeof(client_socket_address));
+	  bind(client_socket, (struct sockaddr *)&client_socket_address, sizeof(client_socket_address));
 
     /**
      * Pack data into buffer
@@ -82,7 +82,7 @@ return_type make_remote_call(const char *servernameorip,
     unsigned char buffer[buffer_size];
     memset(buffer, 0, sizeof(buffer));
 
-	// Pack function name and size in buffer
+	  // Pack function name and size in buffer
   	int function_name_size = sizeof(procedure_name);
     unsigned char *serial_result = int_serialize(buffer, function_name_size);
     unsigned char *function_name = (unsigned char*)procedure_name;
@@ -96,29 +96,30 @@ return_type make_remote_call(const char *servernameorip,
   	serial_result = serial_result + function_name_size;
   	serial_result = int_serialize(serial_result, nparams);
 
-	// Pack argument size and argument in buffer
+	  // Pack argument size and argument in buffer
     va_list var_args;
     va_start(var_args, nparams);
     i = 0;
 
     while (i < nparams) {
         // Put argument size in buffer
-    	int arg_size = va_arg(var_args, int);
-		serial_result = int_serialize(serial_result, arg_size);
+    	  int arg_size = va_arg(var_args, int);
+		    serial_result = int_serialize(serial_result, arg_size);
 
-    	// Put argument in buffer
-    	void *arg = va_arg(var_args, void *);
-    	unsigned char *char_arg = (unsigned char*)arg;
-		printf("Serializing argument: %d \n", *(int*)char_arg);
+    	  // Put argument in buffer
+    	  void *arg = va_arg(var_args, void *);
+    	  unsigned char *char_arg = (unsigned char*)arg;
+		    printf("Serializing argument: %d \n", *(int*)char_arg);
 
         int j = 0;
         while (j < arg_size) {
-			serial_result[j] = char_arg[j];
+			      serial_result[j] = char_arg[j];
             j++;
-		}
-		printf("Sending argument: %d \n", *(int*)serial_result);
+		    }
 
-		serial_result = serial_result + arg_size;
+		    printf("Sending argument: %d \n", *(int*)serial_result);
+
+    		serial_result = serial_result + arg_size;
         i++;
     }
 
@@ -127,40 +128,39 @@ return_type make_remote_call(const char *servernameorip,
     /**
      * Make remote procedure call
      */
-	sendto(client_socket, buffer, sizeof(buffer), 0,
+	  sendto(client_socket, buffer, sizeof(buffer), 0,
 		(struct sockaddr *)&server_socket_address, sizeof(server_socket_address));
 
-	/**
+	  /**
      * Listen for the result of remote procedure call
      */
-	struct sockaddr_in remote_address;
-    socklen_t addrlen = sizeof(remote_address);
+	   struct sockaddr_in remote_address;
+     socklen_t addrlen = sizeof(remote_address);
 
-	unsigned char receive_buffer[buffer_size];
-	while (1) {
-		int receive_length = recvfrom(client_socket, (void *)receive_buffer, sizeof(receive_buffer),
-			0, (struct sockaddr *)&remote_address, &addrlen);
+	   unsigned char receive_buffer[buffer_size];
+	   while (1) {
+		      int receive_length = recvfrom(client_socket, (void *)receive_buffer, sizeof(receive_buffer),
+			        0, (struct sockaddr *)&remote_address, &addrlen);
 
-        if (receive_length > 0) {
-			// Got a good message! Woot!
-			unsigned char *return_value_buffer = receive_buffer + 4;
-            int return_size = *(int*)receive_buffer;
-            unsigned char return_value[return_size];
+          if (receive_length > 0) {
+			        // Got a good message! Woot!
+			        unsigned char *return_value_buffer = receive_buffer + 4;
+              int return_size = *(int*)receive_buffer;
+              unsigned char return_value[return_size];
 
-            int k = 0;
-            while (k < return_size) {
-				return_value[k] = return_value_buffer[k];
-			    k++;
-            }
+              int k = 0;
+              while (k < return_size) {
+		  	          return_value[k] = return_value_buffer[k];
+		  	          k++;
+              }
 
-			return_type rt;
-			memset((unsigned char *)&rt, 0, sizeof(rt));
-			rt.return_val = return_value;
-            rt.return_size = return_size;
-
-			return rt;
-		}
-	}
+		  	      return_type rt;
+		  	      memset((unsigned char *)&rt, 0, sizeof(rt));
+		  	      rt.return_val = return_value;
+              rt.return_size = return_size;
+		  	      return rt;
+		      }
+	   }
 }
 
 int main() {
